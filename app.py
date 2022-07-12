ed7850176015f29e92b604a1f2d8a2568e163ad4
Mateus <mateusc.rossetto@hotmail.com>
2022-07-12 15:17:48 -0300

    feat: energy

--- app.py
+++ app.py
@@ -4,45 +4,56 @@ import matplotlib.pyplot as plt
 delta_t = 0.01
 x0 = 2
 v0 = 2
-steps = 10**4
+steps = 20**3
+
+
+k = 10
+m = 10
 
 
 def a(x):
-    k = 10
-    m = 10
     return - k * x / m
 
 
 def verlet_method(steps, delta_t, x0, v0):
 
-    x1 = x0 + v0 * delta_t
     delta_t2 = delta_t * delta_t
+    x1 = x0 + v0 * delta_t + 0.5 * a(x0) * delta_t2
 
     xt_old = x0
     xt = x1
 
-    position = [x0, x1]
+    position = [x0]
     velocity = [v0]
-    time = [delta_t, 2*delta_t]
+    time = [delta_t]
 
     for i in range(2, steps - 2):
         x_next = 2 * xt - xt_old + a(xt) * delta_t2
         vt = (x_next - xt_old) / (2 * delta_t)
 
-        position.append(x_next)
-        time.append(i * delta_t)
+        position.append(xt)
+        time.append((i - 1) * delta_t)
         velocity.append(vt)
         xt_old = xt
         xt = x_next
 
-    vt = (x_next - xt_old) / (2 * delta_t)
-    velocity.append(vt)
-
     return {"x": position, "t": time, "v": velocity}
 
 
+def Energy(v, x):
+    energy = []
+
+    for i in range(len(v)):
+        Et = (0.5 * k * (x[i] ** 2)) + (0.5 * m * (v[i] ** 2))
+        energy.append(Et)
+
+    return energy
+
+
 verlet = verlet_method(steps, delta_t, x0, v0)
+energy = Energy(verlet["v"], verlet["x"])
 
 plt.plot(verlet["t"], verlet["x"])
 plt.plot(verlet["t"], verlet["v"])
+plt.plot(verlet["t"], energy)
 plt.show()
